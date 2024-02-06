fba458a2bf741d2c4acfb96bfa06112f6cfbd48c
Paul Johann Flade <paul-johann@flade-privat.de>
2024-02-06 11:45:28 +0100

    callenge solved

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
+
+  function handleClickUp() {
+    setCount(count + 1);
+  }
+
+  function handleClickDown() {
+    setCount(count - 1);
+  }
 
   return (
     <div className="container">
       <h1>{count}</h1>
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickDown}>
           -
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("🤔");
-          }}
-        >
+        <button type="button" onClick={handleClickUp}>
           +
         </button>
       </div>
